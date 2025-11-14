10a64a5cb1d26e0f22976801ca403faab3bcaffa
Miss Komal H. Avhad <avhadkomal679@gmail.com>
2025-11-14 11:49:19 +0530

    Add files via upload

--- footer.php
+++ footer.php
@@ -1,13 +1,32 @@
-<!-- Footer Section Starts -->
-<div class="footer">
-    <div class="wrapper">
-        <p class="text-center">
-            2025 © Online Food Ordering System <br>
-            Developed by <b>Komal Avhad & Ganesh Shejul</b> <br>
-            Trimurti Arts, Commerce and Science College, Dhorjalgaon-Ne
+<!-- social Section Starts Here -->
+<section class="social">
+    <div class="container text-center">
+        <ul>
+            <li>
+                <a href="#"><img src="https://img.icons8.com/fluent/50/000000/facebook-new.png"/></a>
+            </li>
+            <li>
+                <a href="#"><img src="https://img.icons8.com/fluent/48/000000/instagram-new.png"/></a>
+            </li>
+            <li>
+                <a href="#"><img src="https://img.icons8.com/fluent/48/000000/twitter.png"/></a>
+            </li>
+        </ul>
+    </div>
+</section>
+<!-- social Section Ends Here -->
+
+<!-- footer Section Starts Here -->
+<section class="footer">
+    <div class="container text-center">
+        <p>
+            © 2025 Online Food Ordering System | 
+            Developed by <strong>Komal Avhad & Ganesh Shejul</strong> <br>
+            Trimurti Arts, Commerce & Science College, Dhorjalgaon-Ne, Tal. Shegaon, Dist. Ahilyanagar
         </p>
     </div>
-</div>
-<!-- Footer Section Ends -->
-    </body>
+</section>
+<!-- footer Section Ends Here -->
+
+</body>
 </html>
--- menu.php
+++ menu.php
@@ -1,33 +1,54 @@
 <?php 
-    include('../config/constants.php'); 
-    include('login-check.php');
+include('config/constants.php'); 
+
+// Start session if not started
+if(session_status() === PHP_SESSION_NONE){
+    session_start();
+}
 ?>
 
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-    <title>Food Order Website - Home Page</title>
-    <link rel="stylesheet" href="../css/admin.css">
-    <link rel="stylesheet" href=
-"https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
-        integrity=
-"sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
-        crossorigin="anonymous"> 
+    <meta charset="UTF-8">
+    <!-- Important to make website responsive -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Foodhub</title>
+
+    <!-- Link our CSS file -->
+    <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
-    <!-- Menu Section Starts -->
-    <div class="menu text-center">
-        <div class="wrapper">
-            <ul>
-                <li><a href="index.php">Home</a></li>
-                <li><a href="manage-users.php">Users</a></li>
-                <li><a href="manage-category.php">Category</a></li>
-                <li><a href="manage-food.php">Food</a></li>
-                <li><a href="manage-order.php">Order</a></li>
-                <li><a href="logout.php">Logout</a></li>
-            </ul>
+    <!-- Navbar Section Starts Here -->
+    <section class="navbar">
+        <div class="container">
+            <!-- Logo -->
+            <div class="logo">
+                <a href="<?php echo SITEURL; ?>" title="Logo">
+                    <img src="images/logo.png" alt="Restaurant Logo" class="img-responsive">
+                </a>
+            </div>
+            <br>
+            <!-- Menu -->
+            <div class="menu text-right">
+                <ul>
+                    <li><a href="<?php echo SITEURL; ?>">Home</a></li>
+                    <li><a href="<?php echo SITEURL; ?>categories.php">Categories</a></li>
+                    <li><a href="<?php echo SITEURL; ?>foods.php">Foods</a></li>
+
+                    <?php if(empty($_SESSION["u_id"])) { ?>
+                        <!-- If not logged in -->
+                        <li><a href="login.php">Login</a></li>
+                    <?php } else { ?>
+                        <!-- If logged in -->
+                        <li><a href="myorders.php">My Orders</a></li>
+                        <li><a href="logout.php" class="btn-danger">Logout</a></li>
+                    <?php } ?>
+                </ul>
+            </div>
+
+            <div class="clearfix"></div>
         </div>
-    </div>
-    <!-- Menu Section Ends -->
-</body>
-</html>
+    </section>
+    <!-- Navbar Section Ends Here -->
